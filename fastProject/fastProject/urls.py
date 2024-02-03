273c3093724634ad4a3f15557af1701dafd8a770
muntasir-i01 <muntasirm55@gmail.com>
2024-02-03 17:20:53 +0600

    import os on settings.py

--- fastProject/fastProject/urls.py
+++ fastProject/fastProject/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('calc.urls')), #for home page you have to look for calc.urls 
+    path('', include('calc.urls')), #for home page you have to look for calc.urls , 
     path('admin/', admin.site.urls),
 ]
