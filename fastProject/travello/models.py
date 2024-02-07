dd06c460777e55ae63d9f3bb5b4000e8b2dfacca
muntasir-i01 <muntasirm55@gmail.com>
2024-02-07 15:59:30 +0600

    passing dynamic data in html by creating object in view.py and creating models in models.py

--- fastProject/travello/models.py
+++ fastProject/travello/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+#model1-class with 5 variable
+class Destination:
+    id: int
+    name: str
+    img: str
+    desc: str
+    price: int
+
--- fastProject/travello/views.py
+++ fastProject/travello/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {'price': 300, 'pric': 400})
+
+    #creating object for destination
+
+    dest0 = Destination()
+    dest0.price = 100
+    dest0.img = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fmountain-bike-bicycle-activity-4642560%2F&psig=AOvVaw2Y31W6Fb6UIKlGmDVvkTPq&ust=1707385278538000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMiwxoD4mIQDFQAAAAAdAAAAABAD'
+
+    dest1 = Destination()
+    dest1.name = 'Running Cycle'
+    dest1.price = 400
+    dest1.desc = 'A run cycle is a sequence of drawings or frames in an animation that show a character running or jogging.'
+
+    dest2 = Destination()
+    dest2.price = 300
+    dest2.desc = 'The great majority of modern bicycles have a frame with upright seating that looks much like the first chain-driven bike.'
+    #dest0.img = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fmountain-bike-bicycle-activity-4642560%2F&psig=AOvVaw2Y31W6Fb6UIKlGmDVvkTPq&ust=1707385278538000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMiwxoD4mIQDFQAAAAAdAAAAABAD'
+
+
+
+    return render(request, 'index.html', {'dest1': dest1, 'dest0': dest0, 'dest2': dest2})
