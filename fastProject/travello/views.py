976a063fdf15de93ea780fcdc71e408ec44b5373
muntasir-i01 <muntasirm55@gmail.com>
2024-02-08 10:01:23 +0600

    add foreach loop in index.html for multiple products and pass value as list from views

--- fastProject/travello/views.py
+++ fastProject/travello/views.py
@@ -7,19 +7,22 @@ def index(request):
     #creating object for destination
 
     dest0 = Destination()
-    dest0.price = 100
-    dest0.img = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fmountain-bike-bicycle-activity-4642560%2F&psig=AOvVaw2Y31W6Fb6UIKlGmDVvkTPq&ust=1707385278538000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMiwxoD4mIQDFQAAAAAdAAAAABAD'
+    dest0.name = 'Kids Cycle'
+    dest0.price = 200
+    dest0.desc =  'Purchase a kids bicycle of excellent quality for safe cycling for your baby. In 2023, the best childrens bicycle at the best price will be delivered.'
+    dest0.img = 'img-4.png'
 
     dest1 = Destination()
     dest1.name = 'Running Cycle'
     dest1.price = 400
     dest1.desc = 'A run cycle is a sequence of drawings or frames in an animation that show a character running or jogging.'
+    dest1.img = 'img-1.png'
 
     dest2 = Destination()
     dest2.name = 'Modern Cycle'
     dest2.price = 300
     dest2.desc = 'The great majority of modern bicycles have a frame with upright seating that looks much like the first chain-driven bike.'
-    dest0.img = 'img.png'
+    dest0.img = 'img-3.png'
 
     dests = [dest0, dest1, dest2]
 
