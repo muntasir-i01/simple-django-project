ad6eb21ad4d8fe1abdf7f3efaa9a4d2198cb9087
muntasir-i01 <muntasirm55@gmail.com>
2024-02-11 11:56:12 +0600

    psycopg2 installed

--- fastProject/fastProject/settings.py
+++ fastProject/fastProject/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'fastProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fastproject',
+        'USER': 'employees',
+        'PASSWORD': 'admin12',
+        'HOST': 'localhost'
     }
 }
 
